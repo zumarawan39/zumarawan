6e667ada6e91f98bb90517ecee179fddcc677a03
zumarawan <zumar.awan@aigros.com>
2025-07-08 18:41:34 +0500

    fix: reponame change

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/NextjsPortfolio' : '',
-  assetPrefix: isProd ? '/NextjsPortfolio/' : '',
+  basePath: isProd ? '/zumarawan' : '',
+  assetPrefix: isProd ? '/zumarawan/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
