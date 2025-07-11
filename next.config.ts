e6451c80b80597cf7a4a227ccb250ccc163582a2
zumarawan <zumar.awan@aigros.com>
2025-07-11 17:33:26 +0500

    feat: config update

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,16 @@
-import { NextConfig } from 'next';
+
+import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/libs/i18n.ts');
-
-const isProd = process.env.NODE_ENV === 'production';
-
+ 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/zumarawan/home' : '',
-  assetPrefix: isProd ? '/zumarawan/home/' : '',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   images: {
-    domains: ['images.prismic.io'],
+    domains: [
+      "images.prismic.io",
+      // agar aur bhi domains hain toh yahan add karen
+    ],
   },
 };
 
